dea3d2e2562bae9af0de608ad53f7384c14abcb8
Rob Szumski <rob@robszumski.com>
2018-05-26 21:11:18 -0700

    types: stub out spec and status

--- pkg/apis/prometheus/v1alpha1/types.go
+++ pkg/apis/prometheus/v1alpha1/types.go
@@ -21,9 +21,60 @@ type PrometheusReplica struct {
 	Status            PrometheusReplicaStatus `json:"status,omitempty"`
 }
 
+
+// Spec
+
 type PrometheusReplicaSpec struct {
-	// Fill me
+	// Size is the size of the memcached deployment
+	Size string          `json:"configMap"`
+	HighlyAvailable bool `json:"highlyAvailable"`
+	BaseDomain string    `json:"baseDomain"`
+	BucketSecret string  `json:"bucketSecret"`
+	Metrics string       PrometheusMetricsSpec `json:"metrics"`
 }
+
+type PrometheusMetricSpec struct {
+	// Size is the size of the memcached deployment
+	Retention string   `json:"retention"`
+	BlockDuration bool `json:"blockDuration"`
+}
+
+// Status
+
 type PrometheusReplicaStatus struct {
-	// Fill me
+	Phase string  `json:"phase"`
+	Ouput         PrometheusReplicaStatus `json:"output,omitempty"`
+	Local         PrometheusReplicaStatus `json:"local,omitempty"`
 }
+
+type PrometheusOutputStatus struct {
+	Grafana string `json:"grafana"`
+	Query string   `json:"query"`
+}
+
+type PrometheusLocalStatus struct {
+	Stores string       `json:"grafana"`
+	Prometheuses string `json:"prometheuses"`
+	Queries string      `json:"queries"`
+}
+
+// spec:
+// 	config: ...
+// 	ha: true
+// 	baseDomain: ...
+// 	metrics:
+// 		retention: 24h
+// 		blockDuration: 2m
+// 	bucketSecret: ...
+// status:
+//   phase: running, creating, deleting
+// 	output:
+// 		grafana: g.e.com or svc.local
+// 		query: q.e.com or svc.local
+// 	local:
+// 		stores:
+// 			...
+// 		prometheuses:
+// 			...
+// 		queries:
+// 		  ...
